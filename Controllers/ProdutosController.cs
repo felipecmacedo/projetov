3814ff98b3d52a1caf38311aa27f310eb8f2d95b
Felipe Cimardi Macedo <felipecimardimacedo97@gmail.com>
2023-06-04 14:26:13 -0300

    Update Program

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,9 +1,10 @@
-﻿using Google.Cloud.Firestore;
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 using ProjetoV.Models;
 
-namespace ProjetoV.Controllers
+namespace ProjetoV.Controllers 
 {
     public class ProdutosController : Controller
     {
@@ -13,32 +14,76 @@ namespace ProjetoV.Controllers
 
         public ProdutosController()
         {
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATON_CREDENTIALS", diretorio);
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", diretorio);
             projetoId = "projetov-e92f8";
             _firestoreDb = FirestoreDb.Create(projetoId);
         }
-
+ 
         public async Task<IActionResult> Index()
         {
             Query produtosQuery = _firestoreDb.Collection("produtos");
             QuerySnapshot produtosQuerySnapshot = await produtosQuery.GetSnapshotAsync();
-            List<Produtos> listaProdutos = new List<Produtos>();
+            List<Produto> listaProdutos = new List<Produto>();
 
             foreach(DocumentSnapshot documentSnapshot in produtosQuerySnapshot.Documents)
             {
-                if(documentSnapshot.Exists)
+                if(documentSnapshot.Exists) 
                 {
                     Dictionary<string, object> produto = documentSnapshot.ToDictionary();
 
                     string json = JsonConvert.SerializeObject(produto);
-                    Produtos novoProduto = JsonConvert.DeserializeObject<Produtos>(json);
+                    Produto novoProduto = JsonConvert.DeserializeObject<Produto>(json);
                     novoProduto.ProdutoId = documentSnapshot.Id;
 
                     listaProdutos.Add(novoProduto);
                 }
             }
-
             return View(listaProdutos);
+        }
+
+        [HttpGet]
+        public IActionResult NovoProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NovoProduto(Produto produto)
+        {
+            CollectionReference collectionReference = _firestoreDb.Collection("produtos");
+            await collectionReference.AddAsync(produto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarProduto(string produtoId)
+        {
+            DocumentReference documentReference = _firestoreDb.Collection("produtos").Document(produtoId);
+            DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
+
+            if (documentSnapshot.Exists)
+            {
+                Produto produto = documentSnapshot.ConvertTo<Produto>();
+                return View(produto);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AtualizarProduto(Produto produto)
+        {
+            DocumentReference documentReferente = _firestoreDb.Collection("produtos").Document(produto.ProdutoId);
+            await documentReferente.SetAsync(produto, SetOptions.Overwrite);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExcluirProduto(string produtoId)
+        {
+            DocumentReference documentReference = _firestoreDb.Collection("produtos").Document(produtoId);
+            await documentReference.DeleteAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using Google.Cloud.Firestore;
+using System.ComponentModel;
+
+namespace ProjetoV.Models
+{
+    [FirestoreData]
+    public class Produto
+    {
+        [DisplayName("Código do Produto")]
+        public string ProdutoId { get; set; }
+
+        [FirestoreProperty]
+        public string Nome { get; set; }
+
+        [FirestoreProperty]
+        public double Preco { get; set; }
+
+        [FirestoreProperty]
+        public int Quantidade { get; set; }
+    }
+}
